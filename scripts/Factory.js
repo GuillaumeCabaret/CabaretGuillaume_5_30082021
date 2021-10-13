27cdd77c30110be4e4b2a1bc7c90fd15a5614159
guillaume Cabaret <cabaretguillaume96@gmail.com>
2021-10-13 14:22:59 +0200

    add profile page + video & photo classes

---
+++ scripts/Factory.js
@@ -0,0 +1,13 @@
+import { Video } from './video.js';
+import { Photo } from './photo.js';
+
+export class Factory {
+
+    static createMedia(mediaInfo) {
+        if ("video" in mediaInfo) {
+            return new Video(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title, mediaInfo.video, mediaInfo.tags, mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt);
+        } else {
+            return new Photo(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title, mediaInfo.image, mediaInfo.tags, mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt)
+        }
+    }
+}
